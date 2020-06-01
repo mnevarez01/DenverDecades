ecbeb5dbb96b4dd2cde6b5c014ed1d95f3531ebb
Kim Tompkins <kimberli.tompkins@outlook.com>
2020-06-01 10:39:09 -0600

    backend routes and seeds

---
+++ client/build/precache-manifest.cb1ccadd740c5db30c593e46730f82e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e95f6849a3b05204980",
+    "url": "/static/css/main.b3d6725b.chunk.css"
+  },
+  {
+    "revision": "4e95f6849a3b05204980",
+    "url": "/static/js/main.535c4b64.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7df09a61a2c37d2f3a9",
+    "url": "/static/js/2.5c25870c.chunk.js"
+  },
+  {
+    "revision": "3da68ecc4fef4a09e259eafe6f092fac",
+    "url": "/index.html"
+  }
+];
--- models/edit.js
+++ models/edit.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const editsSchema = new Schema({
-  content_id: { type: int, required: true },
+  content_id: { type: Number, required: true },
   userName: { type: String, required: true },
   date: { type: Date, default: Date.now }
 });
 
-const Edit = mongoose.model("Edit", editSchema);
+const Edit = mongoose.model("Edit", editsSchema);
 
 module.exports = Edit;
