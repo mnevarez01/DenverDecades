75b071b02cb2e9d36c16f42bac9222656f0716f3
Vanessa Pham <phamxvanessa@gmail.com>
2020-06-01 11:40:59 -0600

    added assets folder

---
+++ client/src/assets/jss/material-kit-react/views/componentsSections/carouselStyle.js
@@ -0,0 +1,16 @@
+import { container } from "assets/jss/material-kit-react.js";
+
+const carouselStyle = {
+  section: {
+    padding: "-40px 0 70px 0",
+  },
+  container,
+  marginAuto: {
+    marginTop: -50,
+    marginBottom: 10,
+    marginLeft: "auto !important",
+    marginRight: "auto !important",
+  },
+};
+
+export default carouselStyle;
