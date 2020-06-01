96dab1343cbc300a6437bed2c82e6f58763b0db4
Vanessa Pham <phamxvanessa@gmail.com>
2020-06-01 14:27:42 -0600

    front end shenanigans

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import { createBrowserHistory } from "history";
+import Nav from "./views/Components/Nav";
 
+import "./assets/scss/material-kit-react.scss?v=1.9.0";
+
+// pages for this product
+import Components from "./views/Components";
+import LandingPage from "./views/LandingPage/LandingPage.js";
+import LoginPage from "./views/LoginPage/LoginPage.js";
+
+var hist = createBrowserHistory();
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Nav />
-        
-        <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Router history={hist}>
+    <Nav />
+    <Switch>
+      <Route path="/landing-page" component={LandingPage} />
+      <Route path="/login-page" component={LoginPage} />
+      <Route path="/" component={Components} />
+    </Switch>
+  </Router>
   );
 }
 
