f01f05720ac85943655a9e65c102d440d0936666
Kim Tompkins <kimberli.tompkins@outlook.com>
2020-06-02 13:54:53 -0600

    removing book

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Books from "./pages/Books";
-// import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import Footer from "./components/footer";
@@ -10,8 +8,10 @@ import HorizontalTimeline from "./components/timeline";
 import HeroBanner from "./components/HeroBanner";
 import Section from "./components/Section";
 
+
+
 // CAN WE HAVE A COMPONENT (vertical timeline) IN ANOTHER COMPONENT (container)
-import MainBody from "./components/MainBody";
+import MainBody from "./components/MainBody"; 
 import './App.css';
 
 function App() {
@@ -20,28 +20,14 @@ function App() {
       <div>
         <Nav />
         <HiddenHeader />
-
         <HeroBanner />
-
+        <Section />
         <HorizontalTimeline />
-
         <Section />
-
-        <Section />
-
         <MainBody />
-
         <Switch>
-
-          {/* <Route exact path={["/", "/books"]}>
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route> */}
           <Route>
             <NoMatch />
-
           </Route>
         </Switch>
         <Footer />
