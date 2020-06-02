087f5da847d6b9ee747ed437fbabea4727c496b6
Maria Nevarez <mnevarez1220@gmail.com>
2020-06-02 17:02:25 -0600

    added li items

---
+++ client/src/components/Display/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Display({ year, title, content }) {
+  <div>
+    <ul>
+      <li>
+        {year, title, content}
+      </li>
+
+    </ul>
+
+  </div>
+
+
+}
+
+export default Display;
