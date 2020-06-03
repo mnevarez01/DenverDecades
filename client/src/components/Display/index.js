22afee06b69d9c1ad89f81a95e73aa291414b1a2
Maria Nevarez <mnevarez1220@gmail.com>
2020-06-03 08:47:07 -0600

    updated timeline

--- client/src/components/Display/index.js
+++ client/src/components/Display/index.js
@@ -1,20 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Timeline, Event } from "react-timeline-scribble"
 
 function Display({ info }) {
   // console.log(props)
   return (
     <div className="container">
 
-      {info.map(({ content, title, year, _id }) => (
-        <div classNames="container">
-          <h3>Year: {year}</h3>
-          <ul className="list-group" key={_id}>
-            <h5> <strong>{title}</strong></h5>
-            <li>
-              <strong>What Happened:</strong> {content}
-            </li>
-          </ul>
-        </div>
+      {info.map(({ content, title, year, _id, author }) => (
+        <Fragment>
+          <Timeline >
+            <Event key={_id} interval={year} title={title} subtitle={author}>
+              {content}
+            </Event>
+          </Timeline>
+        </Fragment>
       ))}
 
     </div>
@@ -23,3 +22,13 @@ function Display({ info }) {
 }
 
 export default Display;
+
+// {/* <div classNames="container">
+//           <h3>Year: {year}</h3>
+//           <ul className="list-group" key={_id}>
+//             <h5> <strong>{title}</strong></h5>
+//             <li>
+//               <strong>What Happened:</strong> {content}
+//             </li>
+//           </ul>
+//         </div> */}
