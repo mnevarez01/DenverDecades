48e8f2e984095f3f025d60b1fba866af0313e28f
Vanessa Pham <phamxvanessa@gmail.com>
2020-06-03 16:14:46 -0600

    styled editform

--- client/src/components/MainBody/index.js
+++ client/src/components/MainBody/index.js
@@ -3,15 +3,8 @@ import { Container } from "../Grid";
 // import { useAuth0 } from "../../react-auth0-spa"
 // import ContentForm from '../ContentForm'
 import Display from '../Display'
-// import { makeStyles } from '@material-ui/core/styles';
-// import './index.css';
-// import image1850 from '../../Assets/Images/1870.jpg'
-// import image1860 from '../../Assets/Images/1880.jpg'
-
 
 function MainBody({ info, decade }) {
-  // const classes = useStyles();
-  // const { isAuthenticated } = useAuth0();
 
   return (
     <Container data-kitten={true} className='display' fluid style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/Images/${decade}.${decade === 1900 || decade === 1910 ? 'png' : 'jpg'})` }} >
@@ -20,7 +13,6 @@ function MainBody({ info, decade }) {
           <div class="bg-dark p-4 rounded shadow">
       <Display info={info} />
       </div></div></div>
-
     </Container>
   );
 }
