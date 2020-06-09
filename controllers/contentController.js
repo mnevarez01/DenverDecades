f26e8aecdeeb7627bc9321ef6b4014d1f24f4583
Maria Nevarez <mnevarez1220@gmail.com>
2020-06-09 11:44:22 -0600

    fixed sorting issue

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -2,47 +2,47 @@ const db = require("../models");
 
 // Defining methods for the ContentsController
 module.exports = {
-  findAll: function(req, res) {
+  findAll: function (req, res) {
     db.Content
       .find(req.query)
       .sort({ year: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  findByRange: function(req, res) {
+  findByRange: function (req, res) {
     db.Content
-      .find({ year: {$gte: req.query.start, $lte: req.query.end}})
-      .sort({ year: -1 })
+      .find({ year: { $gte: req.query.start, $lte: req.query.end } })
+      .sort({ year: 1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  findByDate: function(req, res) {
-    
+  findByDate: function (req, res) {
+
     db.Content
-      .find({ year: req.params.year})
-      .sort({ date: -1 })
+      .find({ year: req.params.year })
+      .sort({ date: 1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  findById: function(req, res) {
+  findById: function (req, res) {
     db.Content
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  create: function(req, res) {
+  create: function (req, res) {
     db.Content
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  update: function(req, res) {
+  update: function (req, res) {
     db.Content
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  remove: function(req, res) { 
+  remove: function (req, res) {
     db.Content
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
